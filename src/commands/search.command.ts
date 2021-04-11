05f06c7a951645c429f4a0642d7df5c761394008
Judy Tuan <judytuna@gmail.com>
2021-04-11 02:58:42 -0700

    Move all content to field value

--- src/commands/search.command.ts
+++ src/commands/search.command.ts
@@ -26,7 +26,7 @@ export class SearchCommand extends BaseCommand {
         }
         const responseMsg = new Discord.RichEmbed().setTitle(`Guild Bank Inventory - Search Result for '${searchString}'`);
         result.forEach(r => {
-            responseMsg.addField(`${r.quantity}x`, `[${r.name}](https://classic.wowhead.com/item=${r.id}) \n ${r.characters}`);
+            responseMsg.addField(`-`, `**[${r.name}](https://classic.wowhead.com/item=${r.id})** \n ${r.quantity}x \n ${r.characters}`);
             responseMsg.setThumbnail(`https://wow.zamimg.com/images/wow/icons/large/${r.icon}.jpg`);
         });
         await message.channel.send(responseMsg);
