11e1beab8cd1fb6eb3d5ec2a2c8a19897797eb72
Judy Tuan <judytuna@gmail.com>
2021-04-13 03:28:10 -0700

    wrap it in a try block

--- src/commands/import.command.ts
+++ src/commands/import.command.ts
@@ -15,27 +15,31 @@ export class ImportCommand extends BaseCommand {
     public usage = `${prefix}${this.name} PASTED_IMPORT_STRING`
 
     public async action(message: Message, args: string[]) {
-      console.log('MESSAGE ATTACHMENT:');
-      console.log(message.attachments);
-      console.log('wwwwwwwwwwwwwww the first ones url is...');
-      console.log(message.attachments.first().url);
-      let importString = args[0];
-      if (importString.length == 0) {
-        console.log('DDDDDDDDDDDDDDDDD empty string found, look at url here')
-        // let file = message.attachments.first().file;
-        // fs.readFile(file, (err, data) => {
-        //   importString = data;
-        // })
+      try {
+        console.log('MESSAGE ATTACHMENT:');
+        console.log(message.attachments);
+        console.log('wwwwwwwwwwwwwww the first ones url is...');
+        console.log(message.attachments.first().url);
+        let importString = args[0];
+        if (args.length == 0) {
+          console.log('DDDDDDDDDDDDDDDDD empty string found, look at url here')
+          // let file = message.attachments.first().file;
+          // fs.readFile(file, (err, data) => {
+          //   importString = data;
+          // })
+        }
+        const decodedArray = atob(importString).split(";");
+        const characterName = decodedArray[0].match(/(\w+)/)[0];
+        const numSlots = decodedArray.length - 3; // 0 is charname, 1 is list of bags, -1 is empty string
+        console.log(`about to import character ${characterName} with ${numSlots} slots`);
+
+        const account = await this.getAccount();
+        const result = await new ApiRequest().forAccount(account).postImportString(importString);
+
+        await message.channel.send(`Imported ${characterName}: ${numSlots} inventory slots.`);
+      } catch(error) {
+        await message.channel.set(`Error! ${error}`);
       }
-      const decodedArray = atob(importString).split(";");
-      const characterName = decodedArray[0].match(/(\w+)/)[0];
-      const numSlots = decodedArray.length - 3; // 0 is charname, 1 is list of bags, -1 is empty string
-      console.log(`about to import character ${characterName} with ${numSlots} slots`);
-
-      const account = await this.getAccount();
-      const result = await new ApiRequest().forAccount(account).postImportString(importString);
-
-      await message.channel.send(`Imported ${characterName}: ${numSlots} inventory slots.`);
     }
 }
 
